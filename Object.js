36ad18796e311d3ffbf22318bbe7bf15af5ad953
Makvana Sagar <134779010+MakvanaSagar@users.noreply.github.com>
2025-08-05 21:41:50 +0530

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,37 @@
+
+// ==========> IMP FOR INTERVIEW : "SHALLOW COPY & DEEP COPY AND HOW TO IT'S USE.." <=======
+
+
+/*
+
+//Example - 1
+
+let myObj = {
+	name: "Mr. Sagar D Makvana",
+	age: 19,
+	weigth: 77,
+	city: "Rajkot",
+}
+console.log(myObj);
+
+
+
+//Example - 2	(Object into function)
+
+let myObj = {
+	name: "Mr. Sagar D Makvana",
+	age: 19,
+	weigth: 77,
+	city: "Rajkot",
+	
+	greet: function()
+	{
+		console.log("Hey everyyon");
+	}
+}
+console.log(myObj);
+myObj.greet();
+
+console.log(typeof(myObj));
+
+*/
